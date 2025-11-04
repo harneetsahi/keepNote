ebd512d08ba545b1774c192fb4ffbfb114a692e2
Harneet <harneetsahi8@gmail.com>
2025-11-03 20:19:23 -0600

    update: remove theme from navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import { NotesIcon } from "../icons/NotesIcon";
 import { Theme } from "./Theme";
+import { Button } from "./Button";
 
 export function Navbar() {
   return (
     <>
-      <div className="">
-        <Link to="/" className="absolute top-8 left-8">
+      <div className="absolute top-4 left-4">
+        <Link to="/" className="">
           <NotesIcon className="size-8  cursor-pointer " />
         </Link>
-        <Theme className="w-max absolute top-7 right-8" size="size-5" />
+        {/* <Theme className="w-max absolute top-7 right-8 z-100" size="size-5" /> */}
+        {/* <Link to="/login" className="z-100">
+          <Button
+            variant="primary"
+            text="Log in"
+            size="md"
+            className="w-30 sm:block hidden"
+          />
+        </Link> */}
       </div>
     </>
   );
